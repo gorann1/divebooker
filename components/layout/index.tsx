c946f734170cbd114f1bf3ba94758e42e938ecac
gorann1 <gorandevop@outlook.com>
2023-02-08 22:11:47 +0100

    #navbar_works

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -8,6 +8,7 @@ import useScroll from "@/lib/hooks/use-scroll";
 import Meta from "./meta";
 import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
+import Navbar from "../ui/NavBar";
 
 export default function Layout({
   meta,
@@ -45,17 +46,59 @@ export default function Layout({
               height="30"
               className="mr-2 rounded-sm"
             ></Image>
-            <p>Precedent</p>
+            <p>SCUBAbooker</p>
           </Link>
           <div>
+          <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 
+                        lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+            <Link className="text-sm text-gray-400 hover:text-gray-500" href="/">Home</Link>
+            <li className="text-gray-300">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+              </svg>
+            </li>
+            <Link className="text-sm text-gray-400 hover:text-gray-500" href="/about">About</Link>
+            <li className="text-gray-300">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+              </svg>
+            </li>
+            <Link className="text-sm text-gray-400 hover:text-gray-500" href="/zones">Zones</Link>
+            <li className="text-gray-300">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+              </svg>
+            </li>
+            <Link className="text-sm text-gray-400 hover:text-gray-500" href="/contact">Contact</Link>
+            <li className="text-gray-300">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+              </svg>
+            </li>
+          </ul>
             <AnimatePresence>
               {!session && status !== "loading" ? (
                 <motion.button
-                  className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+                  className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm 
+                  text-gray-900 font-bold  rounded-xl transition duration-200"
                   onClick={() => setShowSignInModal(true)}
                   {...FADE_IN_ANIMATION_SETTINGS}
                 >
                   Sign In
+                </motion.button>
+              ) : (
+                <UserDropdown />
+              )}
+            </AnimatePresence>
+            <AnimatePresence>
+              {!session && status !== "loading" ? (
+                <motion.button
+                  className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-blue-400 hover:bg-gray-100 text-sm 
+                  text-white-900 font-bold rounded-xl transition duration-200"
+                  onClick={() => setShowSignInModal(true)}
+                  {...FADE_IN_ANIMATION_SETTINGS}
+                >
+                  Sign Up
                 </motion.button>
               ) : (
                 <UserDropdown />
---
+++ pages/zones.tsx
@@ -0,0 +1,7 @@
+export default function Zones() {
+  return(
+    <div>
+      <h3>Zones Page</h3>
+    </div>
+  )
+}
